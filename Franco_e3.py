096b27b481c2a43fdc6e371ab3231dca75347d6c
Franncoo <ocnarfjr2@gmail.com>
2018-10-25 16:56:04 +0800

    Activity 3

---
+++ Franco_e3.py
@@ -0,0 +1,23 @@
+import sys
+#print("Input Sustained Winds:", sys.argv[1]
+
+message = input("Please type a " + 
+"comma separeted list of values: ")
+#print("ads")
+#x = sys.argv[5]
+#x = int(sys.argv[5])
+
+values = message.split(",")
+
+print("the first two element " + 
+"of your list are: {} and {}."
+.format(values[0], values[1]))
+
+
+
+x = sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4],sys.argv[5]
+ #total =+ x
+
+
+print("tSum of Squres: " + x)
+
