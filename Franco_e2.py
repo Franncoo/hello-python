9d2b09ab9b65700fc57d6ac835be0f0354fe4924
Franncoo <ocnarfjr2@gmail.com>
2018-10-25 16:40:38 +0800

    Activity 2

--- Franco_e2.py
+++ Franco_e2.py
@@ -6,14 +6,14 @@ x = sys.argv[1]
 x = int(sys.argv[1])
 
 
-if  x == 61:
+if  x >= 61:
     print("Category: Tropical Depression")
-if x == 62 or x <= 88:
+elif x == 62 or x <= 88:
     print ("Category: Tropical Strom")
-if x == 89 or x <=117:
+elif x == 89 or x <=117:
     print ("Category:Severve Tropical Strom")
-if x == 118 or x <=220:
+elif x == 118 or x <=220:
     print ("Category:Typhoon")
-if x >=220:
+elif x >=220:
     print ("Category:Super Typhoon")
 
